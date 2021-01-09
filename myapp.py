d7b938907eb6018015c3069b313e447c0416f48e
Jared <jared.w.early@gmail.com>
2021-01-09 16:20:03 -0500

    Added simple stock price data app

---
+++ myapp.py
@@ -0,0 +1,28 @@
+import yfinance as yf
+import streamlit as st
+import pandas as pd
+
+# To run this file:
+# Open command prompt, cd to file
+# > streamlit run filename.py
+
+# This write fucntion allows us to write in markdown
+st.write("""
+## Simple Stock Price App
+
+Shown are the stock closing price and volume of Google!
+
+""")
+
+# Define ticker symbol
+tickerSymbol = 'GOOGL'
+
+# Get data on this ticker
+tickerData = yf.Ticker(tickerSymbol)
+
+# Get the historical prices for this ticker
+tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
+# Open  High    Low Close   Volume  Dividends   Stock Splits
+
+st.line_chart(tickerDf.Close)
+st.line_chart(tickerDf.Volume)
